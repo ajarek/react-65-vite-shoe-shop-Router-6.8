b668a8f6c34e02cb02d5ee321dfa239a1b43ff38
Ajarek <ajarek@poczta.onet.pl>
2023-03-14 21:32:25 +0100

    added react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Main />,
+    loader: mainLoader,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Dashboard />,
+        loader: dashboardLoader,
+        // action: dashboardAction,
+        errorElement: <Error />,
+      },
+    ],
+  },
+])
 function App() {
-  return <div className='App'>lol</div>
+  return (
+    <div className='App'>
+      <RouterProvider router={router} />
+    </div>
+  )
 }
 
 export default App
